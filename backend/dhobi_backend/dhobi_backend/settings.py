397b846f75ea690dc100e35e7f067a1d1ababcb5
Arpan Raj <yash42086@gmail.com>
2025-07-15 13:00:39 +0530

    backend deployed

--- backend/dhobi_backend/dhobi_backend/settings.py
+++ backend/dhobi_backend/dhobi_backend/settings.py
@@ -44,7 +44,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["dhobi-backend.onrender.com", "localhost", "127.0.0.1"]
 
 # Application definition
 
